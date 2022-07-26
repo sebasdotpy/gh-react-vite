c7de5245a32c17e95f681de4eb5fa98e7e32a12e
Sebastian Aguirre <sebasaguirresolano@gmail.com>
2022-07-26 00:15:23 -0500

    FIX bug

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://sebasdotpy.github.io/',
+  base: 'https://sebasdotpy.github.io/gh-react-vite',
   plugins: [react()]
 })
